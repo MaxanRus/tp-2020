cb3b116bd0110bba25e16bd2c75150ad28341398
MaxanRus <timosha1113@yandex.ru>
2021-02-19 16:55:21 +0300

    добавил скрипт

---
+++ task1/run.sh
@@ -0,0 +1,20 @@
+path=$1
+ext=$2
+backup=$3
+backupzip=$4
+
+mkdir -p $backup
+
+files=`find $path -name "*$ext"`
+
+length=`expr length $path`
+
+#echo "Длина пути до папки: $length"
+
+for i in $files
+do
+	mkdir -p `dirname $backup/${i:length}`
+	cp $i "$backup/${i:length}"
+done
+
+tar -czf $backupzip $backup
